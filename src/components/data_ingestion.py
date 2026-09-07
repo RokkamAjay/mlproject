a17053be9085efc934029eb123523c8a012d6198
RokkamAjay <ajayrokkam38@gmail.com>
2026-09-07 11:35:55 +0530

    Data Transformation Done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,8 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass ### Decerator
 class DataIngestionConfig:
@@ -34,7 +36,7 @@ class DataIngestion:
             logging.info('Ingestion of the Data is Completed')
 
             return (
-                self.Ingestion_config.train_data_path,
+                self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
             )
         except Exception as e:
@@ -42,4 +44,7 @@ class DataIngestion:
 
 if __name__ == '__main__':
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
